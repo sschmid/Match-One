5167715cc0a8b7f5ad4e448ca1604d6286cc9444
Simon Schmid <mail@sschmid.com>
2015-05-18 12:40:12 +0200

    Added despawn animation and new graphics

--- Assets/Sources/Features/RenderResource/RemoveViewSystem.cs
+++ Assets/Sources/Features/RenderResource/RemoveViewSystem.cs
@@ -1,5 +1,6 @@
 ﻿using Entitas;
 using UnityEngine;
+using DG.Tweening;
 
 public class RemoveViewSystem : IReactiveSystem, ISetPool {
     public IMatcher GetTriggeringMatcher() {
@@ -15,7 +16,14 @@ public class RemoveViewSystem : IReactiveSystem, ISetPool {
     }
 
     void onEntityWillBeRemoved(Group group, Entity entity) {
-        Object.Destroy(entity.view.gameObject);
+        var gameObject = entity.view.gameObject;
+        var spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        var color = spriteRenderer.color;
+        color.a = 0f;
+        spriteRenderer.material.DOColor(color, 0.2f);
+        gameObject.transform
+            .DOScale(Vector3.one * 1.5f, 0.2f)
+            .OnComplete(() => Object.Destroy(gameObject));
     }
 
     public void Execute(Entity[] entities) {
