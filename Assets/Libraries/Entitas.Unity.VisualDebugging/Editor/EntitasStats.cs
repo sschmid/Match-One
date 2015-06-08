8c3128783a7ae9453073af8f190e03a3ddf33639
Simon Schmid <mail@sschmid.com>
2015-06-08 21:53:21 +0200

    Using latest Entitas

---
+++ Assets/Libraries/Entitas.Unity.VisualDebugging/Editor/EntitasStats.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Entitas.CodeGenerator;
+using UnityEditor;
+using UnityEngine;
+
+namespace Entitas.Unity.VisualDebugging {
+    public static class EntitasStats {
+
+        [MenuItem("Entitas/Stats")]
+        public static void LogStats() {
+            foreach (var stat in GetStats()) {
+                Debug.Log(stat.Key + ": " + stat.Value);
+            }
+        }
+
+        public static Dictionary<string, int> GetStats() {
+            var types = Assembly.GetAssembly(typeof(Entity)).GetTypes();
+            var components = types.Where(implementsComponent).ToArray();
+            var pools = getPools(components);
+
+            var stats = new Dictionary<string, int> {
+                { "Components", components.Length },
+                { "Systems", types.Count(implementsSystem) }
+            };
+
+            foreach (var pool in pools) {
+                stats.Add(pool.Key, pool.Value);
+            }
+
+            return stats;
+        }
+
+        static Dictionary<string, int> getPools(Type[] components) {
+            return components.Aggregate(new Dictionary<string, int>(), (lookups, type) => {
+                var lookupTag = type.PoolName();
+                if (lookupTag == string.Empty) {
+                    lookupTag = "DefaultPool";
+                }
+                if (!lookups.ContainsKey(lookupTag)) {
+                    lookups.Add(lookupTag, 0);
+                }
+
+                lookups[lookupTag] += 1;
+                return lookups;
+            });
+        }
+
+        static bool implementsComponent(Type type) {
+            return type.GetInterfaces().Contains(typeof(IComponent))
+                && type != typeof(IComponent)
+                && type != typeof(DebugComponent);
+        }
+
+        static bool implementsSystem(Type type) {
+            return type.GetInterfaces().Contains(typeof(ISystem))
+                && type != typeof(ISystem)
+                && type != typeof(IStartSystem)
+                && type != typeof(IExecuteSystem)
+                && type != typeof(IReactiveSystem)
+                && type != typeof(ReactiveSystem)
+                && type != typeof(Systems)
+                && type != typeof(DebugSystems);
+        }
+    }
+}
+
