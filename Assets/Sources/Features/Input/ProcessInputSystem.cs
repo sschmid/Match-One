511235b7333493c98c248bb21046583807725b4f
Simon Schmid <mail@sschmid.com>
2016-03-10 23:55:46 +0100

    Added check if input position is in the gameboard

--- Assets/Sources/Features/Input/ProcessInputSystem.cs
+++ Assets/Sources/Features/Input/ProcessInputSystem.cs
@@ -17,12 +17,20 @@ public class ProcessInputSystem : IReactiveSystem, ISetPool {
 
         var inputEntity = entities.SingleEntity();
         var input = inputEntity.input;
-        var e = _pool.gameBoardCache.grid[input.x, input.y];
-        if (e != null && e.isInteractive) {
-            e.isDestroy = true;
+
+        if (isInGameboard(input.x, input.y)) {
+            var e = _pool.gameBoardCache.grid[input.x, input.y];
+            if (e != null && e.isInteractive) {
+                e.isDestroy = true;
+            }
         }
 
         _pool.DestroyEntity(inputEntity);
+    }
+
+    bool isInGameboard(int x, int y) {
+        return (x >= 0 && x < _pool.gameBoard.columns)
+            && (y >= 0 && y < _pool.gameBoard.rows);
     }
 }
 
