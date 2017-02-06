69d882d87504fb6ba8444daeb82c8dfa7f122a30
Simon Schmid <mail@sschmid.com>
2017-02-06 15:31:10 +0100

    Using Entitas EntityLink

--- Assets/Sources/Logic/View/Systems/RemoveViewSystem.cs
+++ Assets/Sources/Logic/View/Systems/RemoveViewSystem.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using DG.Tweening;
 using Entitas;
+using Entitas.Unity;
 using UnityEngine;
 
 public sealed class RemoveViewSystem : ReactiveSystem<GameEntity> {
