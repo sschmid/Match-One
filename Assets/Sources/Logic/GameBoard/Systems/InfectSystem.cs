db076a1935a4fd1428f09aa783da4f10ea7c70e7
Ethan Kennerly <ethan@lumoslabs.com>
2017-11-25 15:33:40 -0800

    Infect System: Stubs by copying fall system.

--- Assets/Sources/Logic/GameBoard/Systems/InfectSystem.cs
+++ Assets/Sources/Logic/GameBoard/Systems/InfectSystem.cs
@@ -1,6 +1,29 @@
+using System.Collections.Generic;
+using Entitas;
+
 // Ethan:
 // TODO: As in an auto-linker game like Collapse:
 // Optionally destroy all contiguous colors that match the color.
-// Responds
+// Reacts after a cell is destroyed but before any cell has fallen.
 //
 // This new feature tests how quickly I can edit gameplay in an entity-component-system.
+public sealed class InfectSystem : ReactiveSystem<GameEntity> {
+
+    readonly GameContext _context;
+
+    public InfectSystem(Contexts contexts) : base(contexts.game) {
+        _context = contexts.game;
+    }
+
+    protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context) {
+        return context.CreateCollector(GameMatcher.GameBoardElement.Removed());
+    }
+
+    protected override bool Filter(GameEntity entity) {
+        return true;
+    }
+
+    protected override void Execute(List<GameEntity> entities) {
+        // TODO
+    }
+}
