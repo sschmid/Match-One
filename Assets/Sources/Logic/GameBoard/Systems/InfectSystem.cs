55e0c4be7c68ecbf9c260da61f6b4f3ca9b71992
Ethan Kennerly <ethan@lumoslabs.com>
2017-11-25 15:25:25 -0800

    Infect System: Comments what to-do.

---
+++ Assets/Sources/Logic/GameBoard/Systems/InfectSystem.cs
@@ -0,0 +1,6 @@
+// Ethan:
+// TODO: As in an auto-linker game like Collapse:
+// Optionally destroy all contiguous colors that match the color.
+// Responds
+//
+// This new feature tests how quickly I can edit gameplay in an entity-component-system.
